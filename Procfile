96a2b9866df1b7ff4c47baa64eff01baa7ba8946
Karl Mose <karl.h.mose@gmail.com>
2021-07-12 22:00:46 +0100

    Refactor

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rowingManager.wsgi --log-file -
--- manager/urls.py
+++ manager/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.welcome),
