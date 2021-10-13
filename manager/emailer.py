36b5032e0600e8396452814e3cf9bf1751c206fa
Karl Mose <karl.h.mose@gmail.com>
2021-10-13 20:51:38 +0100

    Updated emailer

--- manager/emailer.py
+++ manager/emailer.py
@@ -23,7 +23,16 @@ def sendSignupDetails(crsid, password):
         "Content-Type: text/html"]
     headers = "\r\n".join(headers)
 
-    body = "Welcome! Sign it at http://secbc.herokuapp.com using "+password+" as your password. Please remember to change your password after you've logged in"
+    body = """<html>
+      <head></head>
+      <body>
+      <p>Welcome to the SECBC website! Sign in at http://secbc.herokuapp.com using """+password+""" as your password.</p>
+      <p>Please remember to change your password after you've logged in</p>
+      <p>HOW TO SIGN UP FOR ERG SESSIONS: Go to login -> Sign up for outings as a rower -> Click 'join' on any erg times for which you may be available</p>
+      <p>Once you've marked yourself as available, the captains may assign you a spot on any of your available days.</p>
+      <p>TO SEE IF YOU'VE BEEN ALLOTED FOR AN ERG SESSION: Check 'view my outings' a bit later (We'll also be sending out mails).</p>
+      </body></html>
+      """
 
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sent_from, email_password)
