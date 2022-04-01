0fe7a837362ccb397df5eee17512deb933b746b8
Karl Mose <karl.h.mose@gmail.com>
2022-04-01 18:22:39 +0100

    Email login fix

--- rowingManager/settings.py
+++ rowingManager/settings.py
@@ -61,7 +61,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'secbc.web@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_KEY')
 
 TEMPLATES = [
     {
